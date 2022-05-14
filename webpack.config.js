1da8d1b47741b707b27920ebc0d68dbe302cca39
Mac clarenz Tamang <macclarenz@gmail.com>
2022-05-14 17:36:44 +0800

    upload via VS Code

--- webpack.config.js
+++ webpack.config.js
@@ -75,10 +75,6 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: path.resolve(__dirname, './src/index.html')
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'addNewCampground.html',
-            template: path.resolve(__dirname, './src/addNewCampground.html')
         })
     ],
     devtool: 'source-map',
